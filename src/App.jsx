cb8b4b53a55494ad147d25e24d772f789a6a5dc5
NewtonKamau <kamaunewton78@gmail.com>
2023-02-03 13:44:17 +0300

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from "react-dom";
-import Pet from './Pet';
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
   const App = () => {
-  <div>
+    return (
+    <div>
     <h1>Adopt Me</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanesse" />
-    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="cat" breed="Mixed" />
+    <SearchParams />
   </div>
+  )
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
