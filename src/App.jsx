920331aeee49bc8afc4251d9054bb91cae260614
NewtonKamau <kamaunewton78@gmail.com>
2023-02-08 16:39:25 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { QueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter,Link,Route,Routes } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
-const queryClient = new QueryClient({
+let queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
