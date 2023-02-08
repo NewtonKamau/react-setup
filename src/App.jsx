b553f815661064a3ed783b6a9add8e857c1645a0
NewtonKamau <kamaunewton78@gmail.com>
2023-02-08 13:09:28 +0300

    use Link to Details and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter,Link,Route,Routes } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
   const App = () => {
     return (
       <BrowserRouter>
-        <h1>Adopt Me</h1>
+      <header> <Link to="/"><h1>Adopt Me</h1></Link></header>
         <Routes>
           <Route path="/" element={<SearchParams />} />
           <Route path="/details/:id" element={<Details />} />          
